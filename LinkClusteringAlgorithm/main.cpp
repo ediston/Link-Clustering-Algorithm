7ef74da3a02010c59325c70c74964fbe8357f5fd
ediston <vivek4dhiman@gmail.com>
2015-02-13 12:02:22 +0100

    Update main.cpp

--- LinkClusteringAlgorithm/main.cpp
+++ LinkClusteringAlgorithm/main.cpp
@@ -1,21 +1,25 @@
-/* 
+/*
  * File:   main.cpp
  * Author: vivekdhiman
  *
- * Created on 12 February, 2015, 10:10 PM
+ * Created on 12 December, 2014, 10:10 PM
  */
-
 #include <cstdlib>
-
+#include <iostream>
+#include "Graph.h"
 using namespace std;
 
 /*
- * 
+ *
  */
 int main(int argc, char** argv) {
-    
-    
-    
+    Graph graph;
+    vector < pair<ll, ll> > links(78);
+    int a,b;
+    while(cin >> a >> b){
+       links.push_back(make_pair(a,b));
+    }
+    graph.setLinks(links);
     return 0;
 }
 
