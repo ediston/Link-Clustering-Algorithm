4845856bf948dcac93cf7f2fb3bc5666115cd3fb
ediston <vivek4dhiman@gmail.com>
2015-02-13 12:01:14 +0100

    Update Graph.h

--- LinkClusteringAlgorithm/Graph.h
+++ LinkClusteringAlgorithm/Graph.h
@@ -1,8 +1,8 @@
-/* 
+/*
  * File:   Graph.h
  * Author: vivekdhiman
  *
- * Created on 12 February, 2015, 10:34 PM
+ * Created on 12 December, 2014, 10:34 PM
  */
 
 #ifndef GRAPH_H
@@ -10,21 +10,51 @@
 
 #include "Node.h"
 #include "Link.h"
+#include <queue>
+
 
 class Graph {
 public:
     Graph();
-    Graph(const Graph& orig);
+    void setLinks(vector < pair<ll, ll> > links);
     virtual ~Graph();
-    double getSimilarity(Node);
+    pair<ll, ll>   getNextMostSimilar();
     void calculateSimilarities();
+    int getNeighbours(int a);
+    int getCommonNeighbours(int a, int b);
 private:
+
     double threshold;
-    map < pair<long long> , double> similarity;
-    vector <Link> links;
-    set < set<int > > clusters;
-    int totalLinks;
-    int totalCommunities;
+    priority_queue < pair<double, pair<ll, ll> >  > similarityQ; // between two links with shared node
+    vector < pair<ll, ll> > allLinks;
+    ll totalLinks;
+    ll totalCommunities;
+
+    //--- Union set ----
+    vector <long long> community,  rank ;
+    void initUnionFind(ll N) {
+        rank.assign(N, 0);
+        community.assign(N, 0);
+        for (ll i = 0; i < N; i++)
+            community[i] = i;
+    }
+    ll findCommunity(ll i) {
+        return (community[i] == i) ? i : (community[i] = findCommunity(community[i]));
+    }
+    bool isSameCommunity(ll i, ll j) {
+        return findCommunity(i) == findCommunity(j);
+    }
+    void unionCommunity(ll i, ll j) {
+        if (!isSameCommunity(i, j)) { // if from different set
+            ll x = findCommunity(i), y = findCommunity(j);
+            if (rank[x] > rank[y])
+                community[y] = x; // rank keeps the tree short
+            else {
+                community[x] = y;
+                if (rank[x] == rank[y]) rank[y]++;
+            }
+        }
+    }
 };
 
 #endif	/* GRAPH_H */
