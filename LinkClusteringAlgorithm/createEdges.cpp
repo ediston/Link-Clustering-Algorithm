bd1cc5c8a92ca1bf5789fe17997dffa0626463be
ediston <vivek4dhiman@gmail.com>
2015-03-24 00:56:43 +0100

    Update createEdges.cpp

--- LinkClusteringAlgorithm/createEdges.cpp
+++ LinkClusteringAlgorithm/createEdges.cpp
@@ -1,9 +1,11 @@
 #include<set>
+#include <vector>    // std::random_shuffle
+#include <iostream>    // std::random_shuffle
 #include <algorithm>    // std::random_shuffle
 #include <ctime>        // std::time
 #include <cstdlib>      // std::rand, std::srand
-#define N 10
-#define M 5
+#define N 1000000
+#define M 1000
 using namespace std;
 
 set< pair<long, long> > pairs;
@@ -14,35 +16,38 @@ int main(){
         std::srand ( unsigned ( std::time(0) ) );
         long i=0;
         for(i=0; i<N; i++)
-                nums.push_back(rand()%i);
+                nums.push_back(rand()%N);
         random_shuffle( nums.begin(), nums.end());
         long totaldone = 0;
         long num = 0;
-        long leftInM = M;
+        long leftInM = N/M;
         long totalNum;
-        long i = 0;
-        while(totaldone<N){
+        i = 0;
+        while(pairs.size()<N){
           if(num%M == M-1){
             totalNum = leftInM;
           }else if(leftInM){
             totalNum = rand()%leftInM;
           }else totalNum = 0;
+         // cout << "totalNum=" << totalNum << endl;
           while(totalNum){
-            if(nums[i] == -1 || nums[i] == num || 
-                  pairs.count(make_pair(num, nums[i]))>0 || 
+            if(nums[i] == -1 || nums[i] == num ||
+                  pairs.count(make_pair(num, nums[i]))>0 ||
                   pairs.count(make_pair(nums[i], num))>0){
-              i = (i+1)%N;
+                        i = (i+1)%N;
+                        continue;
             }
             if(num<nums[i])
-              pairs.count(make_pair(num, nums[i]));
+              pairs.insert(make_pair(num, nums[i]));
             else
-              pairs.count(make_pair(nums[i], num));
+              pairs.insert(make_pair(nums[i], num));
+            //  cout << nums[i] << " " << num << " - " << pairs.size()  << endl;
             nums[i] = -1;
             totalNum--;
             leftInM--;
           }
-          num++;
-          if(num%M == 0) leftInM = M;
+          num = (num+1)%N;
+          if(num%M == 0) leftInM = N/M;
         }
         for(it =pairs.begin(); it!=pairs.end(); it++)
           cout << (*it).first << " " << (*it).second << endl;
