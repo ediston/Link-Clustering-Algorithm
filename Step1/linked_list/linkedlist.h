0536cd895e3037a8b5dc013ca2ae628d900f53ea
ediston <vivek4dhiman@gmail.com>
2015-05-15 13:20:26 +0200

    Update linkedlist.h

--- Step1/linked_list/linkedlist.h
+++ Step1/linked_list/linkedlist.h
@@ -3,4 +3,12 @@
 
 */
 #include <bits/stdc++.h>
+struct node{
+  long long nodeid;
+  node *next;
+}
 
+node *newnode();
+void addNode(long long nodeid);
+node * getLargestSmallerNode();
+long long getCommonNodesCount(node *head1, node *head2);
