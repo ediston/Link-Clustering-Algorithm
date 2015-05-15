bd021ab4e085d6b503d444ffbc7ac7d8db5deca9
ediston <vivek4dhiman@gmail.com>
2015-05-15 13:33:09 +0200

    Update linkedlist.h

--- Step1/linked_list/linkedlist.h
+++ Step1/linked_list/linkedlist.h
@@ -4,11 +4,48 @@
 */
 #include <bits/stdc++.h>
 struct node{
-  long long nodeid;
+  long long nodeId;
   node *next;
+};
+
+node *getNewNode(){
+  node * newnode = new node;
+  newnode->next = NULL;
+  return newnode;
 }
 
-node *newnode();
-void addNode(long long nodeid);
-node * getLargestSmallerNode();
-long long getCommonNodesCount(node *head1, node *head2);
+void addNode(node *head, long long newNodeId){
+  node *tempnode = getNewNode();
+  node *prevnode = getNewNode();
+  prevnode = head;
+  tempnode = head->next;
+  while(tempnode && tempnode->nodeId<newNodeId){
+    prevnode = tempnode;
+    tempnode = tempnode->next;
+  }
+  node*newNode = getNewNode();
+  newNode->nodeId = newNodeId;
+  prevnode->next = newNode;
+  newNode->next = tempnode;
+}
+
+long long getCommonNodesCount(node *head1, node *head2){
+  long long count = 0;
+  node *ll1node = getNewNode();
+  node *ll2node = getNewNode();
+  ll1node = head1->next;
+  ll2node = head2->next;
+  while(ll1node && ll2node){
+    if(ll1node->nodeId<ll2node->nodeId){
+      ll1node = ll1node->next;
+    }else if(ll2node->nodeId<ll1node->nodeId){
+      ll2node = ll2node->next;
+    }else{
+      ll1node = ll1node->next;
+      ll2node = ll2node->next;
+      count++;
+    }
+  }
+  return count;
+}
+
