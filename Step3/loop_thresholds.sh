7b9362fc66ba939bbf9e1cf48007921f4d70ba00
ediston <vivek4dhiman@gmail.com>
2015-07-21 02:31:46 +0200

    Update loop_thresholds.sh

--- Step3/loop_thresholds.sh
+++ Step3/loop_thresholds.sh
@@ -1,27 +1,7 @@
 #!/usr/bin/env bash
 
 # loop_thresholds.sh
-# Jim Bagrow
-# Last Modified: 2009-03-10
-
-
-# Copyright 2009,2010 James Bagrow
-# 
-# 
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-# 
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-# 
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-
+# Vivek Dhiman 
 
 # these need to be updated by you:
 EXEC=./cluster
@@ -30,7 +10,7 @@ JACC=newnetwork.jaccs
 OUTDIR=clusters
 # $EXEC network.pairs network.jaccs network.clusters network.cluster_stats threshold
 
-for thr in 0.9 0.8 0.7 0.6 0.5 0.4 0.3 0.2 0.1
+for thr in 0.9 0.8 0.7 0.6 0.5 0.4 0.3 0.25 0.2 0.15 0.1
 do
     echo $thr
     $EXEC $NET $JACC $OUTDIR/network_$thr.cluster $OUTDIR/network_$thr.cluster_stats $thr > $OUTDIR/runTime_$thr
