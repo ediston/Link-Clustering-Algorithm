037f466ecdb7c22948d16b17881314d4b1eb8748
ediston <vivek4dhiman@gmail.com>
2015-07-24 17:26:46 +0200

    Create createdSortedJaccsFile.cpp

---
+++ Step3/createdSortedJaccsFile.cpp
@@ -0,0 +1,54 @@
+/*
+//  $ g++ -O3 -o createdSortedJaccsFile createdSortedJaccsFile.cpp
+//  $ ./createdSortedJaccsFile network.pairs network.jaccs threshDensity.csv
+*/
+#include <math.h> 
+#include <ctime>
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <set>
+#include <map>
+#include <utility>   // for pairs
+#include <algorithm> // for swap
+#define MIN_DIFF_BW_JACCS 0.0001
+using namespace std;
+
+int main (int argc, char const *argv[]){
+    //************* make sure args are present:
+    if (argc != 3){
+        cout << "ERROR: something wrong with the inputs" << endl;
+        cout << "usage:\n    " << argv[0] << " network.jaccs sortedjaccs.csv" << endl;
+        exit(1);
+    }
+    //************* got the args
+    clock_t begin = clock(); 
+    ifstream jaccFile;  
+    jaccFile.open( argv[1] );
+    if (!jaccFile) {
+        cout << "ERROR: unable to open jaccards file" << endl;
+        exit(1); // terminate with error
+    }
+    int edgeId1,edgeId2; 
+    float jacc;
+    set<float> thresholdSet;
+    while ( jaccFile >> edgeId1 >> edgeId2 >> jacc ) {
+        if(thresholdSet.count(jacc) == 0)
+          thresholdSet.insert(jacc);
+    }
+    jaccFile.close();jaccFile.clear();
+    cout << "Done with thresholdSet creation." << endl;
+    // create the file
+    // if exists then overwrite everything
+    FILE * sortedJaccsFile = fopen( argv[2], "w" );
+    fclose(sortedJaccsFile);
+    for(thIt = thresholdSet.rbegin(); thIt!=thresholdSet.rend(); thIt++){
+      threshold = *thIt;
+      sortedJaccsFile = fopen( argv[2], "a" );
+      fprintf( sortedJaccsFile, "%.6f \n", threshold, D);
+      fclose(sortedJaccsFile);
+    }
+    thresholdSet.clear();
+    cout << "Time taken = " << double(clock() - begin)/ CLOCKS_PER_SEC << " seconds. "<< endl;
+    return 0;
+}
